232c313aad6d568a415e9714a95c121373367467
Dirkjan Ochtman <dirkjan@ochtman.nl>
2022-02-01 10:27:42 +0100

    Remove init() wrapper

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,7 +1,7 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
-    let authentication_manager = gcp_auth::init().await?;
+    let authentication_manager = gcp_auth::AuthenticationManager::new().await?;
     let _token = authentication_manager
         .get_token(&["https://www.googleapis.com/auth/cloud-platform"])
         .await?;
