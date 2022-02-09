92d188ae507d2b29d20b41020a87f313f51655a5
Dirkjan Ochtman <dirkjan@ochtman.nl>
2022-02-09 10:42:49 +0100

    gcloud: retrieve project ID at initialization

--- src/gcloud_authorized_user.rs
+++ src/gcloud_authorized_user.rs
@@ -13,17 +13,14 @@ use crate::Token;
 #[derive(Debug)]
 pub(crate) struct GCloudAuthorizedUser {
     gcloud: PathBuf,
+    project_id: Option<String>,
 }
 
 impl GCloudAuthorizedUser {
     pub(crate) fn new() -> Result<Self, Error> {
-        which("gcloud")
-            .map_err(|_| GCloudNotFound)
-            .map(|path| Self { gcloud: path })
-    }
-
-    fn project_id(&self) -> Result<String, Error> {
-        run(&self.gcloud, &["config", "get-value", "project"])
+        let gcloud = which("gcloud").map_err(|_| GCloudNotFound)?;
+        let project_id = run(&gcloud, &["config", "get-value", "project"]).ok();
+        Ok(Self { gcloud, project_id })
     }
 
     fn token(&self) -> Result<Token, Error> {
@@ -37,7 +34,7 @@ impl GCloudAuthorizedUser {
 #[async_trait]
 impl ServiceAccount for GCloudAuthorizedUser {
     async fn project_id(&self, _: &HyperClient) -> Result<String, Error> {
-        self.project_id()
+        self.project_id.clone().ok_or(Error::NoProjectId)
     }
 
     fn get_token(&self, _scopes: &[&str]) -> Option<Token> {
@@ -73,7 +70,7 @@ mod tests {
     #[ignore]
     fn gcloud() {
         let gcloud = GCloudAuthorizedUser::new().unwrap();
-        println!("{:?}", gcloud.project_id());
+        println!("{:?}", gcloud.project_id);
         println!("{:?}", gcloud.token());
     }
 }
