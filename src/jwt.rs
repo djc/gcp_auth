9d1555f795edde1d80f7a2b952fc7c63eea6cb17
Matthew Helmer <mhelmer@bluecatnetworks.com>
2023-03-24 19:54:14 -0400

    Upgrade base64 dependency and resolve build failures

--- src/jwt.rs
+++ src/jwt.rs
@@ -1,5 +1,6 @@
 //! Copyright (c) 2016 Google Inc (lewinb@google.com).
 
+use base64::{engine::general_purpose::URL_SAFE, Engine};
 use serde::Serialize;
 
 use crate::custom_service_account::ApplicationCredentials;
@@ -8,11 +9,6 @@ use crate::types::Signer;
 
 pub(crate) const GRANT_TYPE: &str = "urn:ietf:params:oauth:grant-type:jwt-bearer";
 const GOOGLE_RS256_HEAD: &str = r#"{"alg":"RS256","typ":"JWT"}"#;
-
-/// Encodes s as Base64
-fn append_base64<T: AsRef<[u8]> + ?Sized>(s: &T, out: &mut String) {
-    base64::encode_config_buf(s, base64::URL_SAFE, out)
-}
 
 /// Permissions requested for a JWT.
 /// See https://developers.google.com/identity/protocols/OAuth2ServiceAccount#authorizingrequests.
@@ -55,13 +51,13 @@ impl<'a> Claims<'a> {
 
     pub(crate) fn to_jwt(&self, signer: &Signer) -> Result<String, Error> {
         let mut jwt = String::new();
-        append_base64(GOOGLE_RS256_HEAD, &mut jwt);
+        URL_SAFE.encode_string(GOOGLE_RS256_HEAD, &mut jwt);
         jwt.push('.');
-        append_base64(&serde_json::to_string(self).unwrap(), &mut jwt);
+        URL_SAFE.encode_string(&serde_json::to_string(self).unwrap(), &mut jwt);
 
         let signature = signer.sign(jwt.as_bytes())?;
         jwt.push('.');
-        append_base64(&signature, &mut jwt);
+        URL_SAFE.encode_string(&signature, &mut jwt);
         Ok(jwt)
     }
 }
