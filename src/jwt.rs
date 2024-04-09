e8e3be3771fe62d15e76106b150c6d44c10fb30d
Dirkjan Ochtman <dirkjan@ochtman.nl>
2024-04-09 11:47:59 +0200

    Rename subject field to sub
    So far this field was not used.

--- src/jwt.rs
+++ src/jwt.rs
@@ -19,7 +19,7 @@ pub(crate) struct Claims<'a> {
     aud: &'a str,
     exp: i64,
     iat: i64,
-    subject: Option<&'a str>,
+    sub: Option<&'a str>,
     scope: String,
 }
 
@@ -27,7 +27,7 @@ impl<'a> Claims<'a> {
     pub(crate) fn new<T>(
         key: &'a ApplicationCredentials,
         scopes: &[T],
-        subject: Option<&'a str>,
+        sub: Option<&'a str>,
     ) -> Self
     where
         T: std::string::ToString,
@@ -45,7 +45,7 @@ impl<'a> Claims<'a> {
             aud: &key.token_uri,
             exp: expiry,
             iat,
-            subject,
+            sub,
             scope,
         }
     }
