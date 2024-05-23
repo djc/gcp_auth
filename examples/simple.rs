d98a0d22d417eb41ec51fbb89bd1c16e6794a1b8
Dirkjan Ochtman <dirkjan@ochtman.nl>
2024-05-23 13:07:49 +0200

    Expose trait implementers directly

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,9 +1,9 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
-    let authentication_manager = gcp_auth::AuthenticationManager::new().await?;
-    let _token = authentication_manager
-        .get_token(&["https://www.googleapis.com/auth/cloud-platform"])
+    let token_provider = gcp_auth::provider().await?;
+    let _token = token_provider
+        .token(&["https://www.googleapis.com/auth/cloud-platform"])
         .await?;
     Ok(())
 }
