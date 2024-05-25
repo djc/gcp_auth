d02fa39d1977585a1811988de94543c6079afd31
Dirkjan Ochtman <dirkjan@ochtman.nl>
2024-05-25 20:50:46 +0200

    Replace env_logger with tracing-subscriber in example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,6 +1,12 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::FmtSubscriber::builder()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .finish(),
+    )
+    .unwrap();
+
     let token_provider = gcp_auth::provider().await?;
     let _token = token_provider
         .token(&["https://www.googleapis.com/auth/cloud-platform"])
